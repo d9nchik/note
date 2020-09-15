a2c48b12fb76742b865cf963323c0dbfcc0c15c9
d9nich <dan.galaiko@gmail.com>
2020-09-15 15:07:11 +0300

    TODOS for next work has been added.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ const NAME_FIELD = document.getElementById('urlName');
 const NOTES_FIELD = document.getElementById('notesNames');
 const NAME_OF_KEYS_ARRAY = 'hesoyamBaguvix';//Easter egg
 const NAME_OF_DATE_ARRAY = 'timeSingularity';
+//TODO: add ability to create notes with same name
+//TODO: fix problem with url
+//TODO: add ability to start from /
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
